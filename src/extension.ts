80c927acacc7142ad900a2d31af3e5986805086a
Aluxray <aluxray693@gmail.com>
2025-03-26 21:12:09 +0100

    feat: added basic sounds handling

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,130 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import path from 'path';
 import * as vscode from 'vscode';
+import { play } from 'sound-play';
+import util from 'util';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const SFX = new Map<string, string>([
+    ['\t', 'tab'],
+    ['\n', 'enter'],
+    ['\r', 'enter'],
+    ['}', 'brace_closed'],
+    ['{', 'brace_open'],
+    [']', 'bracket_closed'],
+    ['[', 'bracket_open'],
+    [')', 'parenthesis_closed'],
+    ['(', 'parenthesis_open'],
+    ['\\', 'slash_back'],
+    ['/', 'slash_forward'],
+    ['&', 'ampersand'],
+    ['*', 'asterisk'],
+    ['@', 'at'],
+    ['?', 'question'],
+    ['!', 'exclamation'],
+    ['~', 'tilde'],
+    ['#', 'pound'],
+    ['%', 'percent'],
+    ['^', 'caret'],
+    ['$', 'dollar'],
+    ['€', 'dollar'],
+    ['£', 'dollar'],
+    ['¥', 'dollar'],
+    ['¢', 'dollar'],
+    ['¤', 'dollar'],
+]);
+
+function playSound({filename, isSfx = false, isMale = false}: {filename: string, isSfx?: boolean, isMale?: boolean}) {
+    let folder: string = '';
+    if (isSfx === true) {
+        folder = 'sfx';
+    } else if (!isNaN(Number(filename))) {
+        folder = 'vocals';
+    } else {
+        folder = 'animalese';
+    }
+
+    let soundPath: string = '';
+    if (isSfx === true) {
+        soundPath = path.join(__dirname, '..', 'assets', 'audio', folder, filename + '.aac');
+    } else {
+        soundPath = path.join(__dirname, '..', 'assets', 'audio', folder, isMale ? 'male' : 'female', 'voice_1', filename + '.aac');
+    }
+
+    console.log(`Played sound ${soundPath}`);
+    play(soundPath).catch((error: unknown) => {
+        console.error('Failed to play sound:', error);
+    });
+}
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "animalese-typing-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+    console.log('Extension loaded!');
+    
 	const disposable = vscode.commands.registerCommand('animalese-typing-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from animalese-typing-vscode!');
+        const soundPath = path.join(__dirname, '..', 'assets', 'audio', 'animalese', 'female', 'voice_1', 'a.aac');
+        play(soundPath).catch((error: unknown) => {
+            console.error('Failed to play sound:', error);
+        });
+        vscode.window.showInformationMessage(`Hello World "${soundPath}"!`);
+	});
+
+	const sound = vscode.workspace.onDidChangeTextDocument((event) => {
+        if (event.contentChanges.length > 0) {
+            const lastChange = event.contentChanges[0].text[0];
+
+            if (lastChange === undefined) {
+                playSound({
+                    filename: 'backspace',
+                    isSfx: true
+                });
+            } else if (/^[a-z]/i.test(lastChange)) {
+                playSound({
+                    filename: lastChange
+                });
+            } else if (/^[0-9-=]/.test(lastChange)) {
+                const num = Number(lastChange);
+                if (num > 0 && num <= 9) {
+                    playSound({
+                        filename: (num-1).toString()
+                    });
+                } else if (num === 0) {
+                    playSound({
+                        filename: '9'
+                    });
+                } else if (lastChange === '-') {
+                    playSound({
+                        filename: '10'
+                    });
+                } else if (lastChange === '=') {
+                    playSound({
+                        filename: '11'
+                    });
+                }
+            } else if (SFX.get(lastChange) !== undefined) {
+                playSound({
+                    filename: SFX.get(lastChange) ?? 'default',
+                    isSfx: true
+                });
+            } else if (lastChange === ' ') {
+                const arrows = ['up', 'down', 'left', 'right'];
+                playSound({
+                    filename: 'arrow_' + arrows[Math.floor(Math.random() * 4)],
+                    isSfx: true
+                });
+            } else {
+                playSound({
+                    filename: 'default',
+                    isSfx: true
+                });
+            }
+        } else {
+            playSound({
+                filename: 'backspace',
+                isSfx: true
+            });
+        }
 	});
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(sound);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
